76aa7d3248ce15a73de6e7c5a209f04428a55775
Erik Michael Swan <erikmswan@gmail.com>
2019-01-04 19:53:29 -0800

    get fonts and style in place

--- src/client/routes/Home/Home.js
+++ src/client/routes/Home/Home.js
@@ -1,9 +1,10 @@
+import './Home.scss';
 import * as React from "react";
 import { hot } from "react-hot-loader";
 
 export const Home = () => (
   <div className="home-container">
-    <h1>In Profundis</h1>
+    <h1 className="title">In Profundis.</h1>
 
     <p>
       As he walked down the black-cobbled street, he marked the faces of the
